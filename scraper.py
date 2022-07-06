f46224b6c46998c00b05de503c3ed12235207e0b
Matthias Clein <m.cleinespinoza@gmail.com>
2022-07-05 23:16:59 -0400

    update to buscalibre scraper + .gitignore update

--- scraper.py
+++ scraper.py
@@ -3,12 +3,17 @@ import lxml.html as html
 import os
 import datetime
 
-HOME_URL = 'https://ideaufro.com/blog/'
-XPATH_LINK_TO_BLOGPOSTS = '//h2[@class="entry-title"]/a/@href'
-XPATH_TITLE = '//h1[@class="entry-title"]/text()'
-XPATH_BODY = '//div[@class="entry-content clear"]/p[not(@class)]/text()'
+books = [
+    "libro-python-data-science-handbook-essential-tools-for-working-with-data-libro-en-ingles/9781491912058/p/48477050",
+    "libro-practical-statistics-for-data-scientists-50-essential-concepts-using-r-and-python-libro-en-ingles/9781492072942/p/52304858",
+    "libro-star-wars-datos-fascinantes/9788411121910/p/54051514"
+]
 
-def parse_blogpost(link, today):
+BOOK_URL = 'https://www.buscalibre.cl/'
+XPATH_TITLE = '//h1/text()'
+XPATH_PRICE = '//p[@class="precioAhora margin-0 font-weight-strong"]/span/text()'
+
+def parse_book(link, today):
     try:
         response = requests.get(link)
         if response.status_code == 200:
@@ -17,39 +22,43 @@ def parse_blogpost(link, today):
             try:
                 title = parsed.xpath(XPATH_TITLE)[0]
                 title = title.replace('\"', '')
-                body = parsed.xpath(XPATH_BODY)
+                price = parsed.xpath(XPATH_PRICE)
             except IndexError:
                 return
-            with open(f'{today}/{title}.txt', 'w', encoding = 'utf-8') as f:
-                f.write(title)
-                f.write('\n\n')
-                for p in body:
-                    f.write(p)
+            if not os.path.isfile(f'resultados/{title}.txt'):
+                with open(f'resultados/{title}.txt', 'w', encoding = 'utf-8') as f:
+                    f.write(title)
+                    f.write('\n')
+                    f.write(f"{today} => {price[0]}")
+                    f.write('\n')
+            else:
+                with open(f'resultados/{title}.txt', 'a', encoding = 'utf-8') as f:
+                    f.write(f"{today} => {price[0]}")
                     f.write('\n')
         else:
             raise ValueError(f'Error: {response.status_code}')
     except ValueError as ve:
         print(ve)
 
-def parse_home():
+def parse_books():
     try:
-        response = requests.get(HOME_URL)
+        response = requests.get(BOOK_URL)
         if response.status_code == 200:
             home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
-            links_to_blogposts = parsed.xpath(XPATH_LINK_TO_BLOGPOSTS)
             today = datetime.date.today().strftime('%d-%m-%Y')
-            if not os.path.isdir(today):
-                os.mkdir(today)
-            for link in links_to_blogposts:
-                parse_blogpost(link, today)
+            if not os.path.isdir("resultados"):
+                os.mkdir("resultados")
+            for book in books:
+                link = BOOK_URL + book
+                parse_book(link, today)
         else:
             raise ValueError(f'Error: {response.status_code}')
     except ValueError as ve:
         print(ve)
 
 def run():
-    parse_home()
+    parse_books()
 
 if __name__ == '__main__':
     run()
